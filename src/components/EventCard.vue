bf4d9dd0061378164faed05f217afd5373f0dc96
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-25 03:00:01 +0100

    setup linting configs

---
+++ src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 v-if @click="handler(arg, event)">Event Card</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: value
+    };
+  }
+};
+</script>
+
+<style></style>
