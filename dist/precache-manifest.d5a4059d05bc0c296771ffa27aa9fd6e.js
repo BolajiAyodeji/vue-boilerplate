aea244d8e8d03e665e46b039ace6c45d0675a85c
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-02-25 02:07:27 +0100

    deployed :zap:

---
+++ dist/precache-manifest.d5a4059d05bc0c296771ffa27aa9fd6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "11e19f4d8d7ac255a9ad",
+    "url": "/js/chunk-vendors.2c17016b.js"
+  },
+  {
+    "revision": "4182fb0164f4a5ffa8c9",
+    "url": "/js/app.09a81644.js"
+  },
+  {
+    "revision": "31bf0e9b39aa3c930fa1",
+    "url": "/js/about.77c93d06.js"
+  },
+  {
+    "revision": "26278762b49bdce82207ddc6c5f846c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4182fb0164f4a5ffa8c9",
+    "url": "/css/app.b7b29f14.css"
+  }
+];
